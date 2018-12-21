75ee772c0301a7540767c3d3c7b50c9c383656ce
Lucas Guasselli <Lucaszack265@gmail.com>
2018-12-21 16:34:34 -0200

    conteúdo do pdf está similar ao do aditamento , porém falta os artigos. Alguns problemas com estrutura da tabela e css não estão sendo reconhecidos pelo pdf.

--- src/app/relatorio/relatorio.component.ts
+++ src/app/relatorio/relatorio.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as jsPDF from 'jspdf';
+import { Militar } from '../militar';
+import { Endereco } from '../endereco';
+import { CrudMilitaresService } from '../crud-militares.service';
+import { Desconto } from '../desconto';
+import { CrudAuxilioTransporteService } from '../crud-auxilio-transporte.service';
 
 @Component({
   selector: 'app-relatorio',
@@ -8,9 +13,18 @@ import * as jsPDF from 'jspdf';
 })
 export class RelatorioComponent implements OnInit {
 
-  constructor() { }
+  militares: Militar[] = [];
+  enderecos: Endereco[] = [];
+  descontos: Desconto[] = [];
+
+
+  constructor(private servicoCrudMilitar: CrudMilitaresService, private servicoCrudAT: CrudAuxilioTransporteService) { }
 
   ngOnInit() {
+      this.militares = this.servicoCrudMilitar.getMilitares();
+      this.enderecos = this.servicoCrudMilitar.getEnderecos();
+      this.descontos = this.servicoCrudAT.getDescontos();
+
   }
 
   // tslint:disable-next-line:member-ordering
@@ -20,6 +34,7 @@ export class RelatorioComponent implements OnInit {
 
       let doc = new jsPDF();
 
+
       let specialElementHandlers = {
           '#editor': function(element, renderer) {
               return true;
@@ -28,8 +43,8 @@ export class RelatorioComponent implements OnInit {
 
       let content = this.content.nativeElement;
 
-      doc.fromHTML(content.innerHTML, 15, 15, {
-          'width': 190,
+      doc.fromHTML(content.innerHTML, 3, 3, {
+          'width': 922 ,
           'elementHandlers': specialElementHandlers
       });
 
