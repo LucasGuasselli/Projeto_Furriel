1e19d0e7169cab76cfee7fe3af80996d7cd1aa04
Lucas Guasselli <Lucaszack265@gmail.com>
2019-05-16 17:37:42 -0300

    mostrando militares recebidos na requisição no HTML

--- src/app/tabela-militares/tabela-militares.component.ts
+++ src/app/tabela-militares/tabela-militares.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Militar } from '../militar';
 import { Endereco } from '../endereco';
 import { CrudMilitaresService } from '../crud-militares.service';
+import { MilitarDTO } from '../models/MilitarDTO';
 
 @Component({
   selector: 'app-tabela-militares',
@@ -12,16 +13,17 @@ export class TabelaMilitaresComponent implements OnInit {
 
   titulo = 'Tabela de Militares';
 
-  militares: Militar[] = [];
+  militares: MilitarDTO[] = [];
+  // militares: Militar[] = [];
   enderecos: Endereco[] = [];
 
   constructor(private servico: CrudMilitaresService) { }
 
   ngOnInit() {
-      this.militares = this.servico.getMilitares();
+     // this.militares = this.servico.getMilitares();
       this.enderecos = this.servico.getEnderecos();
       this.servico.findAll().subscribe(response => {
-        console.log(response);
+        this.militares = response;
       });
   }
 
