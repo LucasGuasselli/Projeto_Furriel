7f2f02d8be25561a7c44b1627f19735807245c3d
Lucas Guasselli <Lucaszack265@gmail.com>
2019-09-06 18:49:55 -0300

    utilizando Angular material.

--- src/app/tabelas-component/tabela-aditamentos/tabela-aditamentos.component.ts
+++ src/app/tabelas-component/tabela-aditamentos/tabela-aditamentos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AditamentosService } from '../../services/aditamentos.service';
 import { AditamentoDTO } from '../../models/aditamento.dto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabela-aditamentos',
@@ -10,11 +11,22 @@ import { AditamentoDTO } from '../../models/aditamento.dto';
 export class TabelaAditamentosComponent implements OnInit {
 
   aditamentos: AditamentoDTO[] = [];
-  constructor(private aditamentosService: AditamentosService) { }
+  displayedColumns: string[] = ['nome', 'data'];
+
+  constructor(private aditamentosService: AditamentosService,
+              private router: Router) { }
 
   ngOnInit() {
       this.aditamentosService.findAll().subscribe( response => {this.aditamentos = response; },
        error => { console.log(error); });
+  }
+
+  moveToFormAditamento() {
+    this.router.navigate(['/cadastroAditamento']);
+  }
+
+  moveToIndex() {
+    this.router.navigate(['/index']);
   }
 
 }
