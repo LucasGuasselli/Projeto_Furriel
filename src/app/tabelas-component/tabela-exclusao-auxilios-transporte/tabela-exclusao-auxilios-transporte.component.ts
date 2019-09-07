b14d51d62b0aff988fdaf46c50e04f2997bbf933
Lucas Guasselli <Lucaszack265@gmail.com>
2019-09-07 11:45:49 -0300

    Utilizando angular material e arrumando a requisição das exclusões de auxílio transporte ao back-end.

--- src/app/tabelas-component/tabela-exclusao-auxilios-transporte/tabela-exclusao-auxilios-transporte.component.ts
+++ src/app/tabelas-component/tabela-exclusao-auxilios-transporte/tabela-exclusao-auxilios-transporte.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ExclusaoAuxilioTransporteDTO } from '../../models/exclusaoAuxilioTransporte.dto';
 import { ExclusoesAuxilioTransporteService } from '../../services/exclusaoAuxilioTransporte.service';
+import { MilitaresService } from '../../services/militares.service';
+import { PostosGraduacoesService } from '../../services/postosGraduacoes.service';
+import { Router } from '@angular/router';
+import { MilitarDTO } from '../../models/militar.dto';
+import { PostoGraduacaoDTO } from '../../models/postoGraduacao.dto';
 
 @Component({
   selector: 'app-tabela-exclusao-auxilios-transporte',
@@ -10,13 +15,48 @@ import { ExclusoesAuxilioTransporteService } from '../../services/exclusaoAuxili
 export class TabelaExclusaoAuxiliosTransporteComponent implements OnInit {
 
   exclusaoAuxiliosTransporte: ExclusaoAuxilioTransporteDTO[] = [];
-  constructor(private servicoExclusaoAuxilioTransporte: ExclusoesAuxilioTransporteService) { }
+  militares: MilitarDTO[] = [];
+  postoGraduacao: PostoGraduacaoDTO;
+
+  displayedColumns: string[] = ['graduacaoNome', 'militarPrecCP', 'data', 'motivo', 'valor'];
+
+  constructor(private exclusoesAuxilioTransporteService: ExclusoesAuxilioTransporteService,
+              private militaresService: MilitaresService,
+              private postosGraduacoesService: PostosGraduacoesService,
+              private router: Router) { }
 
   ngOnInit() {
-    //  this.exclusaoAuxiliosTransporte = this.servicoExclusaoAuxilioTransporte.getExclusaoAuxiliosTransporte();
+    this.loadExclusoes();
   }
 
-  removerExclusaoAuxilioTransporte(exclusaoAuxilioTransporte: ExclusaoAuxilioTransporteDTO) {
-   // this.servicoExclusaoAuxilioTransporte.removerExclusaoAuxilioTransporte(exclusaoAuxilioTransporte);
+  loadExclusoes() {
+    this.exclusoesAuxilioTransporteService.findAll().subscribe(response => {this.exclusaoAuxiliosTransporte = response;
+    console.log(this.exclusaoAuxiliosTransporte); this.atribuiMilitares(this.exclusaoAuxiliosTransporte); } ,
+      error => {console.log(error); } );
   }
+
+  atribuiMilitares(exclusaoAuxiliosTransporte: ExclusaoAuxilioTransporteDTO[]) {
+    for (let i = 0; i < exclusaoAuxiliosTransporte.length; i++) {
+        this.militaresService.findMilitarByPrecCP(exclusaoAuxiliosTransporte[i].militarPrecCP).subscribe(
+          response => {this.militares[i] = response; exclusaoAuxiliosTransporte[i].nome = this.militares[i].nome;
+                      this.atribuiGraduacoes(exclusaoAuxiliosTransporte[i], this.militares[i]); },
+          error => {console.log(error); }
+        );
+    }
+  }
+
+  atribuiGraduacoes(exclusaoAuxiliosTransporte: ExclusaoAuxilioTransporteDTO, militar: MilitarDTO) {
+      this.postosGraduacoesService.findPostoGraduacaoById(militar.postoGraduacaoId).subscribe(
+        response => {this.postoGraduacao = response; exclusaoAuxiliosTransporte.graduacao = this.postoGraduacao.nome; },
+           error => {console.log(error); } );
+  }
+
+  moveToFormExclusao() {
+    this.router.navigate(['/cadastroExclusaoAuxilioTransporte']);
+  }
+
+  cancel() {
+    this.router.navigate(['/index']);
+  }
+
 }
