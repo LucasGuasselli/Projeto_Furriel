a5903b667ac05f7453247ba45c75381e5859c726
Lucas Guasselli <Lucaszack265@gmail.com>
2019-11-25 10:59:06 -0300

    Funcionando corretamente oo cadastro de despesa para uma guarnição de serviço

--- src/app/forms-component/form-desconto-guarnicao-servico/form-desconto-guarnicao-servico.component.ts
+++ src/app/forms-component/form-desconto-guarnicao-servico/form-desconto-guarnicao-servico.component.ts
@@ -4,6 +4,10 @@ import { MilitarDTO } from '../../models/militar.dto';
 import { AditamentoDTO } from '../../models/aditamento.dto';
 import { AditamentosService } from '../../services/aditamentos.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { DespesaDTO } from '../../models/despesa.dto';
+import { UtilService } from '../../services/util.service';
+import { DespesasService } from '../../services/despesas.service';
+import { isNull } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-form-desconto-guarnicao-servico',
@@ -12,14 +16,35 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class FormDescontoGuarnicaoServicoComponent implements OnInit {
 
+  // arrays
   militaresComAuxilioTransporte: MilitarDTO[] = [];
+  adjuntos: MilitarDTO[] = [];
   sargentosDeDia: MilitarDTO[] = [];
   cabosDeDia: MilitarDTO[] = [];
   plantoes: MilitarDTO[] = [];
+
+  // objetos selecionados para cadastrar desconto
+  adjunto: MilitarDTO;
+  sgtDeDia: MilitarDTO;
+  caboDeDia: MilitarDTO;
+  plantao: MilitarDTO;
+  plantao1: MilitarDTO;
+  plantao2: MilitarDTO;
+  plantao3: MilitarDTO;
+  plantao4: MilitarDTO;
+  plantao5: MilitarDTO;
+
+  guarnicaoDeServico: MilitarDTO[] = [];
+
+  despesa: DespesaDTO = new DespesaDTO;
   aditamentoAtual: AditamentoDTO;
+  feriado: Boolean = false;
+  administrativo: Boolean = false;
 
   constructor(private militaresService: MilitaresService,
               private aditamentosService: AditamentosService,
+              private despesasService: DespesasService,
+              private utilService: UtilService,
               private router: Router) { }
 
   ngOnInit() {
@@ -41,7 +66,11 @@ export class FormDescontoGuarnicaoServicoComponent implements OnInit {
   // separa os militares por graduacao
   splitMilitares(militares: MilitarDTO[]) {
       for (let i = 0; i < militares.length; i++) {
-        // 2º e 3º Sargentos sao da escala de sargento de dia
+        // 1º Sargentos concorrem a escala de adjunto
+          if (militares[i].postoGraduacaoId === 6) {
+            this.adjuntos.push(militares[i]);
+          }
+        // 2º e 3º Sargentos concorrem a escala de sargento de dia
           if (militares[i].postoGraduacaoId === 5 || militares[i].postoGraduacaoId === 4 ) {
               this.sargentosDeDia.push(militares[i]);
           }
@@ -52,6 +81,96 @@ export class FormDescontoGuarnicaoServicoComponent implements OnInit {
             this.plantoes.push(militares[i]);
           }
       }
+  }
+
+  saveMilitar(militar: MilitarDTO, texto:  String) {
+    switch (texto) {
+      case 'Adjunto': {
+        this.adjunto = militar;
+          break;
+      }
+      case 'Sargento': {
+        this.sgtDeDia = militar;
+          break;
+      }
+      case 'Cabo': {
+        this.caboDeDia = militar;
+          break;
+      }
+      case 'Plantao': {
+        this.plantao = militar;
+          break;
+      }
+      case 'Plantao1': {
+        this.plantao1 = militar;
+          break;
+      }
+      case 'Plantao2': {
+        this.plantao2 = militar;
+          break;
+      }
+    }
+  }
+
+  saveDespesa() {
+    if (this.despesa.calculoDataInicio == null) {
+      alert('Você deve informar a data do Serviço!!!');
+    } else {
+      // tslint:disable-next-line:max-line-length
+      if (this.adjunto != null || this.sgtDeDia != null || this.caboDeDia != null || this.plantao != null || this.plantao1 != null || this.plantao2 != null || this.plantao3 != null || this.plantao4 != null || this.plantao5 != null) {
+        this.modifyDespesaFeriadoAdm();
+
+        this.despesa.quantidadeDias = this.utilService.calculaQuantidadeDias( this.despesa.calculoDataInicio,
+          this.despesa.calculoDataInicio, this.despesa.motivo = 'Serviço' , this.despesa.feriados, this.despesa.administrativos);
+
+        this.despesa.aditamentoId = this.aditamentoAtual.id;
+
+        // e necessario receber as datas antes dos calculos, pois elas podem ser alteradas para os calculos
+          this.despesa.dataInicio = this.utilService.formatDate(this.despesa.calculoDataInicio.toString());
+
+          this.insertDespesa(this.adjunto);
+          this.insertDespesa(this.sgtDeDia);
+          this.insertDespesa(this.caboDeDia);
+          this.insertDespesa(this.plantao);
+          this.insertDespesa(this.plantao1);
+          this.insertDespesa(this.plantao2);
+          this.insertDespesa(this.plantao3);
+          this.insertDespesa(this.plantao4);
+          this.insertDespesa(this.plantao5);
+
+          alert(this.despesa.quantidadeDias + " Dia(s) Descontado(s)");
+      } else {
+        alert('Você deve selecionar no MÍNIMO UM militar!!!');
+      }
+    }
+  }
+
+  insertDespesa(militar: MilitarDTO) {
+    // e necessario o teste de null pois nao tem obrigatoriedade da regra de negocio cadastrar todos os possiveis militares de servico
+    if ( militar != null) {
+      this.despesa.militarPrecCP = militar.precCP;
+
+      this.despesasService.insert(this.despesa).subscribe(response => { console.log(response);
+        if (response.status != 201 ) { alert('Erro ao cadastrar despesa do ' + militar.nome + 'Prec-CP ' + militar.precCP); } } ,
+        error => {console.log(error); });
+    }
+  }
+
+  modifyDespesaFeriadoAdm() {
+    if (this.feriado === true) {
+      this.despesa.feriados = 1;
+      this.despesa.administrativos = 0;
+    }
+    if (this.administrativo === true) {
+      this.despesa.feriados = 0;
+      this.despesa.administrativos = 1;
+    }
+    if (this.feriado === false && this.administrativo === false) {
+      this.despesa.feriados = 0;
+      this.despesa.administrativos = 0;
+    }
+    // console.log('Feriado' + this.despesa.feriados + 'Adm' + this.despesa.administrativos);
+
   }
 
   cancelar() {
