c93285c040bd6f4f38810ba954743b7f7f3663e7
Lucas Guasselli <Lucaszack265@gmail.com>
2020-05-16 10:06:02 -0300

    Inserido janelas (alert) informando que precisa preencher o motivo da atualização de auxílio transporte e também quando deu certo ou não o mesmo cadastro.

--- src/app/forms-component/form-atualizacao-auxilio-transporte/form-atualizacao-auxilio-transporte.component.ts
+++ src/app/forms-component/form-atualizacao-auxilio-transporte/form-atualizacao-auxilio-transporte.component.ts
@@ -102,34 +102,39 @@ export class FormAtualizacaoAuxilioTransporteComponent implements OnInit {
 
   // este metodo e responsavel direcionar a regra de negocio conforme aumento ou queda do valor total do auxílio
   verifyValues() {
-    // variaveis locais
+    // variaveis locais usadas no auxilio das decisões e calculos de valores
       let valoresAntigos = null;
       let valoresConducoes = null;
       let diferenca = null;
 
     if (this.data != null) {
-      for (let i = 0; i < this.valoresAntigos.length; i++) {
-          valoresAntigos += this.valoresAntigos[i];               
-      }
-      for (let i = 0; i < this.conducoes.length; i++) {
-          valoresConducoes += this.conducoes[i].valor;
-      }
-        if (valoresConducoes > valoresAntigos) {
-            // cadastrar saque atrasado
-            diferenca = valoresConducoes - valoresAntigos;
-              this.beforeInsertAtualizacaoAuxilioTransporte(this.auxilioTransporte);
-              this.beforeInsertSaqueAtrasado(this.auxilioTransporte, diferenca);
-        } else if (valoresConducoes < valoresAntigos) {
-            // cadastrar desconto
-            diferenca = valoresAntigos - valoresConducoes;
-            console.log('diferenca: ' + diferenca);
-               this.beforeInsertAtualizacaoAuxilioTransporte(this.auxilioTransporte);
-               this.beforeInsertDespesa(this.auxilioTransporte, diferenca);
-        } else {
-           // valores iguais só altera texto
-        }
+      if (this.atualizacaoAuxilioTransporte.motivo != null) {
+          for (let i = 0; i < this.valoresAntigos.length; i++) {
+                    valoresAntigos += this.valoresAntigos[i];               
+          }
+          for (let i = 0; i < this.conducoes.length; i++) {
+                valoresConducoes += this.conducoes[i].valor;
+          }
+            if (valoresConducoes > valoresAntigos) {
+                // cadastrar saque atrasado
+                  diferenca = valoresConducoes - valoresAntigos;
+                    this.beforeInsertSaqueAtrasado(this.auxilioTransporte, diferenca);
+                    this.beforeInsertAtualizacaoAuxilioTransporte(this.auxilioTransporte);
+            } else if (valoresConducoes < valoresAntigos) {
+                // cadastrar desconto
+                  diferenca = valoresAntigos - valoresConducoes;
+                   console.log('diferenca: ' + diferenca);
+                    this.beforeInsertDespesa(this.auxilioTransporte, diferenca);
+                    this.beforeInsertAtualizacaoAuxilioTransporte(this.auxilioTransporte);
+            } else {
+                 // valores iguais só altera texto
+            }
+      } else{
+                  alert('Você deve inserir um motivo!');
+
+      }      
     } else {
-          alert ('Você deve selecionar uma data inicial!');
+          alert('Você deve selecionar uma data inicial!');
     }      
             // alert('Você não pode inserir um valor menor que o anterior.\n Verifique os valores inseridos!');
   }
@@ -153,20 +158,27 @@ export class FormAtualizacaoAuxilioTransporteComponent implements OnInit {
   }
 
   updateConducoes() {
+    // se alguma conducao der erro ao atualizar 
+    let valida = true;
     for (let  i = 0; i < this.conducoes.length; i++) {
-      // conducoes que sofreram alteracoes
+      // conducoes que sofreram alteracoes (sao validadas por possuir um ID e terem valor diferente do anterior)
         if (this.conducoes[i].id != null && this.valoresAntigos[i] != this.conducoes[i].valor) {
            this.conducoesService.update(this.conducoes[i], this.conducoes[i].id).subscribe(
-              response => { console.log(response); } ,
+              response => { if (response.status != 204) {
+                              valida = false;                            
+                            } 
+              },
                 error => {console.log(error); }
            );
         }
       // adicionando conducoes novas 
         if(this.conducoes[i].id == null && this.conducoes[i].itinerario != null && this.conducoes[i].nomeEmpresa != null && this.conducoes[i].tipoDeTransporte != null && this.conducoes[i].valor > 0) {
-            console.log('passou a validacao');
-              this.conducoes[i].auxilioTransporteId = this.auxilioTransporte.id;
+            this.conducoes[i].auxilioTransporteId = this.auxilioTransporte.id;
                 this.conducoesService.insertNewConducao(this.conducoes[i]).subscribe(
-                  response => {  if (response.status === 201){console.log('conducao nova cadastrada com sucesso!'); }},
+                  response => {  if (response.status != 201){
+                                  valida = false;
+                                }
+                  },
                     error => { console.log(error); }
                 );
         }
@@ -174,11 +186,19 @@ export class FormAtualizacaoAuxilioTransporteComponent implements OnInit {
         if (this.conducoes[i].id != null && this.conducoes[i].valor == 0) {
           console.log('deletando conducao');
             this.conducoesService.delete(this.conducoes[i]).subscribe(
-                response => { if (response.status == 204) { console.log(response); } },
+                response => { if (response.status != 204) { 
+                                valida = false;
+                              } 
+                },
                 error => { console.log(error); }
             );
         }
   }
+    if (valida == true) {
+        alert('Atualização de Auxilio cadastrada com sucesso!');
+    }else {
+        alert('Não foi possivel atualizar corretamente o Auxílio Transporte');
+    }
     // this.router.navigate(['/listaATConducao']);
   }
 
@@ -203,7 +223,7 @@ export class FormAtualizacaoAuxilioTransporteComponent implements OnInit {
   // este metodo serve para cadastrar um saque atrasado referente a inclusao do Auxilio Transporte, tendo em vista que o militar só recebe pelo menos um mes e um dia apos a solicitacao
   insertSaqueAtrasado() {
        this.saquesAtrasadosService.insert(this.saqueAtrasado).subscribe(
-           response => { if(response.status == 201) { console.log( 'Cadastro de saque atrasado com sucesso!' ); console.log(this.saqueAtrasado) } ; },
+           response => { if(response.status == 201) { alert( 'Cadastro de Saque Atrasado com sucesso!' ); } ; },
                 error => { console.log(error); }
        );
   }
