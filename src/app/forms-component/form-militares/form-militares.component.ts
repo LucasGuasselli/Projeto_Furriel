c2cc93252e6a935f443d8e7536be19305833a1be
Lucas Guasselli <Lucaszack265@gmail.com>
2020-05-20 16:43:00 -0300

    Validação para impedir o cadastro duplicado de PrecCP.

--- src/app/forms-component/form-militares/form-militares.component.ts
+++ src/app/forms-component/form-militares/form-militares.component.ts
@@ -17,6 +17,7 @@ export class FormMilitaresComponent implements OnInit {
     titulo = 'Cadastro de Militares';
 
     militar: MilitarDTO = new MilitarDTO();
+    militares: MilitarDTO[] = [];
     endereco: EnderecoDTO = new EnderecoDTO();
     postosGraduacoes: PostoGraduacaoDTO[] = [];
     precCP: number = null;
@@ -31,18 +32,22 @@ export class FormMilitaresComponent implements OnInit {
 
   ngOnInit() {
     this.precCP = this.rota.snapshot.params['cod'];
-    this.postosGraduacoesService.findAll().subscribe(response => {this.postosGraduacoes = response; } ,
-      error => {console.log(error); } );
+    this.postosGraduacoesService.findAll().subscribe(response => { this.postosGraduacoes = response; } ,
+      error => {console.log(error); } 
+    );
+    this.militaresService.findAll().subscribe(response => { this.militares = response; },
+      error => { console.log(error); }
+    );
 
-    /* IF - cadastro ou quando o usuario insere um preccp invalido
-    ELSE - serve para casos de edicao da entidade militar */
+    // casos de cadastro 
       if (isNaN(this.precCP)) {
         this.militar  = new MilitarDTO();
         this.endereco  = new EnderecoDTO();
       } else {
+        // casos de edicao da entidade militar
           alert('O Prec-CP e a Graduação não devem ser alterados!');
           this.loadUpdateMilitarAndEndereco();
-      //    console.log(this.endereco.militarPrecCP);
+       // console.log(this.endereco.militarPrecCP);
       }
   }
 
@@ -57,18 +62,23 @@ export class FormMilitaresComponent implements OnInit {
   // salva  ou edita uma entidade militar e endereco
   validateMilitar() {
     if (isNaN(this.precCP)) {
-        this.precCP = this.militar.precCP;
-            // tslint:disable-next-line:triple-equals
-            if (this.validatePrecCP() == true) {
-              // fazendo com que "o endereco saiba a qual militar pertence"
-                this.endereco.militarPrecCP = this.militar.precCP;
-              this.insertMilitar();
-              // redireciona para a lista
-                  this.router.navigate(['/listaMilitares']);
-            } else {
-              // substituir por uma janela ou pop-up posteriormente
-              alert('valor inválido inserido no campo Prec-CP');
-            }
+      if(this.verificaPrecCPCadastrados(this.militar.precCP) == true) {      
+          this.precCP = this.militar.precCP;
+              // tslint:disable-next-line:triple-equals
+              if (this.validatePrecCP() == true) {
+                // fazendo com que "o endereco saiba a qual militar pertence"
+                  this.endereco.militarPrecCP = this.militar.precCP;
+                this.insertMilitar();
+                // redireciona para a lista
+                    this.router.navigate(['/listaMilitares']);
+              } else {
+                // substituir por uma janela ou pop-up posteriormente
+                  this.precCP = null;
+                alert('valor inválido inserido no campo Prec-CP');
+              }
+      } else {
+         alert('Você inseriu um Prec-CP já Cadastrado!');
+      }
     } else {
       if( this.validatePostoGraduacaoId == this.militar.postoGraduacaoId) {
         console.log('chegou na edicao' + this.precCP);
@@ -78,9 +88,19 @@ export class FormMilitaresComponent implements OnInit {
                 this.router.navigate(['/listaMilitares']);
       } else {
         alert('Você NÃO pode alterar a graduação do militar!!');
-      }
-        
+      }        
     }
+  }
+
+  verificaPrecCPCadastrados(precCP: number) {
+      for (let i = 0; i < this.militares.length; i++) {
+        // se o precCP digitado for igual a de algum militar cadastrado, return FALSE
+         if (this.militares[i].precCP == precCP) {
+            return false;
+         }
+
+      }
+         return true;
   }
 
   validatePrecCP() {
